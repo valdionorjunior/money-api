37515f333c5ee7520a2e00761d5d9a05c22ab1af
valdionorbelts <valdionor.junior@beltsysplus.com.br>
2018-11-17 18:35:10 -0200

    delete e atualizar

--- src/main/java/com/money/api/resource/PessoaResource.java
+++ src/main/java/com/money/api/resource/PessoaResource.java
@@ -3,20 +3,26 @@ package com.money.api.resource;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.money.api.event.RecursoCriadoEvent;
 import com.money.api.model.Pessoa;
 import com.money.api.repository.PessoaRepository;
+import com.money.api.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -27,6 +33,9 @@ public class PessoaResource {
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private PessoaService pessoaService;
 
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
@@ -39,6 +48,18 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
 		Pessoa pessoa = pessoaRepository.findOne(codigo);
 		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}") //deleta pelo cogido
+	@ResponseStatus(HttpStatus.NO_CONTENT) // codigo 204, deu certo mas n tenho nada para retornar
+	public void remover(@PathVariable Long codigo) { // pega codigo da urla passada
+		pessoaRepository.delete(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualiza(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
 	}
 
 }
