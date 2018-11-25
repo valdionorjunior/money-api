c90e3f1b82be6f0b02e8454e46cf2d6c16621f8f
Valdionor Junior <valdionorjunior@outlook.com>
2018-11-25 13:52:54 -0200

    adicionado pesquisa lancamento

--- src/main/java/com/money/api/resource/LancamentoResource.java
+++ src/main/java/com/money/api/resource/LancamentoResource.java
@@ -1,14 +1,19 @@
 package com.money.api.resource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +22,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.money.api.event.RecursoCriadoEvent;
+import com.money.api.exceptionhandler.MoneyExceptionHandler.Erro;
 import com.money.api.model.Lancamento;
 import com.money.api.repository.LancamentoRepository;
+import com.money.api.repository.filter.LancamentoFilter;
+import com.money.api.service.LancamentoService;
+import com.money.api.service.exception.PessoaInexistenteOuInativaException;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -28,11 +37,17 @@ public class LancamentoResource {
 	private LancamentoRepository lancamentoRepository;
 	
 	@Autowired
+	private LancamentoService lancamentoService;
+	
+	@Autowired
 	private ApplicationEventPublisher publisher;
 	
+	@Autowired 
+	private MessageSource messageSource;
+	
 	@GetMapping
-	public List<Lancamento>Lista(){
-		return lancamentoRepository.findAll();
+	public List<Lancamento>pesquisar(LancamentoFilter lancamentoFilter){
+		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
 	
 	@GetMapping("/{codigo}")
@@ -44,10 +59,21 @@ public class LancamentoResource {
 	
 	@PostMapping
 	public ResponseEntity<Lancamento> criaLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
-		Lancamento lancamentoSalva = lancamentoRepository.save(lancamento);
+		Lancamento lancamentoSalva = lancamentoService.salvar(lancamento);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalva.getCodigo()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamento);
 	}
 	
+	//Excpetion não geral, então podemos tratala aqui mesmo
+	@ExceptionHandler({PessoaInexistenteOuInativaException.class})
+	public ResponseEntity<Object>handlerPessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex){
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString(); 
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		
+		return ResponseEntity.badRequest().body(erros);
+		
+	}
+
 }
